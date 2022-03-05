cdc7e73d65aa656747b37c79216702ef58258ecc
Amkam <amaduskamara@gmail.com>
2022-03-05 10:15:20 +0000

    Perform deletion of a doc

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,16 +1,26 @@
-export default function BookList({ books }) {
+import { useEffect } from "react";
 
+// 1. Import firebase db and firestore functions
+import { db } from "../firebase/config";
+import { doc, deleteDoc } from "firebase/firestore";
+
+export default function BookList({ books }) {
+  // In the function, pass in the id of the doc to be deleted
   const handleClick = async (id) => {
-    console.log(id)
-  }
+    // 2. create a docRef and pass in the db, collection name and id of the doc to be deleted that comes from the function
+    const docRef = doc(db, "books", id);
+    await deleteDoc(docRef);
+  };
 
   return (
     <div className="book-list">
       <ul>
-        {books.map(book => (
-          <li key={book.id} onClick={() => handleClick(book.id)}>{book.title}</li>
+        {books.map((book) => (
+          <li key={book.id} onDoubleClick={() => handleClick(book.id)}>
+            {book.title}
+          </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
