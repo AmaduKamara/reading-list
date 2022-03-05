8f15f2a90d59b086e12e1ac4c8304f6b4c29526d
Amkam <amaduskamara@gmail.com>
2022-03-05 10:09:16 +0000

    Add logic for posting data

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,20 +1,31 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+// 1. Import firebase db and firestore functions
+import { db } from "../firebase/config";
+import { collection, addDoc } from "firebase/firestore";
 
 export default function BookForm() {
-  const [newBook, setNewBook] = useState('')
+  const [newBook, setNewBook] = useState("");
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    console.log(newBook)
+    e.preventDefault();
 
-    setNewBook('')
-  }
+    // 2. create a ref of the collection and pass in db and the name of the ref
+    const ref = collection(db, "books");
+
+    // 3. use the addDoc function and pass in ref, and the data object to be added
+    await addDoc(ref, {
+      title: newBook,
+    });
+
+    setNewBook("");
+  };
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
         <span>Add a new book title:</span>
-        <input 
+        <input
           required
           type="text"
           onChange={(e) => setNewBook(e.target.value)}
@@ -23,5 +34,5 @@ export default function BookForm() {
       </label>
       <button>Add</button>
     </form>
-  )
+  );
 }
