40c6e0db4c69f7404810f05da9bcaaf3a81d7bdc
Amkam <amaduskamara@gmail.com>
2022-03-05 09:59:04 +0000

    Add a custom hook to fetch data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,25 @@
-import { useState, useEffect } from "react";
 import BookList from "../components/BookList";
 import BookForm from "../components/BookForm";
 
-import { db } from "../firebase/config";
-import { collection, getDocs } from "firebase/firestore";
+import { useCollection } from "../hooks/useCollection";
+
+// import { db } from "../firebase/config";
+// import { collection, getDocs } from "firebase/firestore";
 
 export default function Home() {
-  const [books, setBooks] = useState(null);
+  const { documents: books } = useCollection("books");
 
-  useEffect(() => {
-    const ref = collection(db, "books");
-    getDocs(ref).then((snapshot) => {
-      let books = [];
-      snapshot.forEach((doc) => {
-        books.push({ id: doc.id, ...doc.data() });
-      });
-      setBooks(books);
-    });
-  }, []);
+  // const [books, setBooks] = useState(null);
+  // useEffect(() => {
+  //   const ref = collection(db, "books");
+  //   getDocs(ref).then((snapshot) => {
+  //     let books = [];
+  //     snapshot.forEach((doc) => {
+  //       books.push({ id: doc.id, ...doc.data() });
+  //     });
+  //     setBooks(books);
+  //   });
+  // }, []);
 
   return (
     <div className="App">
